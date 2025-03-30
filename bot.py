ef7f75216e5b0134c3643d9d2d45a5ca1d30c52d
Fan_tasma <63489501+MrR0b0t19@users.noreply.github.com>
2025-03-30 16:24:15 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import os
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
+from handlers import config
+
+#variables de entorno
+load_dotenv()
+TOKEN = os.getenv("TU_TELEGRAM_TOKEN")
+
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler("start", config.start))
+dispatcher.add_handler(CommandHandler("setdomain", config.set_domain))
+dispatcher.add_handler(CommandHandler("settech", config.set_tech))
+
+print("🤖 Bot iniciado... Esperando comandos.")
+updater.start_polling()
+updater.idle()
 
